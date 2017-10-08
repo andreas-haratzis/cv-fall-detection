cf5d76458e4f498ddaf3f7d7e98cef79172efce1
ADre <andreas.haratzis@gmail.com>
2017-10-08 12:50:44 +1100

    Implemented background seperataion, basic frame cleanup with image deionosation

--- corrections.py
+++ corrections.py
@@ -1,6 +1,26 @@
+import cv2
+from collections import namedtuple
+
+subtractor = cv2.createBackgroundSubtractorKNN()
 
 def init():
     pass
 
 def clean_frame(frame):
-    pass
+    (frame_col, frame_d) = frame
+
+    frame_half = cv2.resize(frame_col, (0,0), fx=0.5, fy=0.5)
+
+    frame_deionized = cv2.fastNlMeansDenoisingColored(frame_half, None, 10, 10, 5, 7)
+    cv2.imshow("Deionized", frame_deionized)
+
+    frame_fg = subtractor.apply(frame_deionized)
+
+    # Remove lil islands from fg mask
+    frame_fg_mask = cv2.GaussianBlur(frame_fg, (5,5), 0, 0)
+    _, frame_fg_mask = cv2.threshold(frame_fg_mask, 192, 255, cv2.THRESH_BINARY)
+    cv2.imshow("Foreground", frame_fg_mask)
+    frame_fg = cv2.bitwise_and(frame_fg, frame_fg, mask=frame_fg_mask)
+
+    Frame = namedtuple('Frame', 'col d half deionized, fg')
+    return Frame(frame_col, frame_d, frame_half, frame_deionized, frame_fg)
