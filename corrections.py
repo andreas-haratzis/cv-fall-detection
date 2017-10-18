6fb30037897a86d4d670daf6baa8dbfa1e9f5991
ADre <andreas.haratzis@gmail.com>
2017-10-18 17:06:16 +1100

    Used means instead of NI, much faster and only slightly worse

--- corrections.py
+++ corrections.py
@@ -1,20 +1,26 @@
 import cv2
 from collections import namedtuple
 
-subtractor = cv2.createBackgroundSubtractorKNN()
+subtractor = None
+
 
 def init():
-    pass
+    global subtractor
+    subtractor = cv2.createBackgroundSubtractorKNN()
+
 
 def clean_frame(frame):
     (frame_col, frame_d) = frame
 
     frame_half = cv2.resize(frame_col, (0,0), fx=0.5, fy=0.5)
 
-    frame_deionized = cv2.fastNlMeansDenoisingColored(frame_half, None, 10, 10, 5, 7)
-    cv2.imshow("Deionized", frame_deionized)
+    frame_median = cv2.medianBlur(frame_half, 5)
+    cv2.imshow("Median", frame_median)
+    frame_fg = subtractor.apply(frame_median)
 
-    frame_fg = subtractor.apply(frame_deionized)
+    #frame_deionized = cv2.fastNlMeansDenoisingColored(frame_half, None, 10, 10, 5, 7)
+    #cv2.imshow("Deionized", frame_deionized)
+    #frame_fg = subtractor.apply(frame_deionized)
 
     # Remove lil islands from fg mask
     frame_fg_mask = cv2.GaussianBlur(frame_fg, (5,5), 0, 0)
@@ -22,5 +28,5 @@ def clean_frame(frame):
     cv2.imshow("Foreground", frame_fg_mask)
     frame_fg = cv2.bitwise_and(frame_fg, frame_fg, mask=frame_fg_mask)
 
-    Frame = namedtuple('Frame', 'col d half deionized, fg')
-    return Frame(frame_col, frame_d, frame_half, frame_deionized, frame_fg)
+    Frame = namedtuple('Frame', 'col d half median fg')
+    return Frame(frame_col, frame_d, frame_half, frame_median, frame_fg)
