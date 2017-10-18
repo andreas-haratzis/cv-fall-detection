ef6059c4a2a9fdf677b39c59a7ec301d2ef47ee4
ADre <andreas.haratzis@gmail.com>
2017-10-18 17:07:29 +1100

    Now provide person history, velocity of rect (averaged over last 4 frames), used means instead of NI for optical flow, show movement history, improved velocity estimation using masks

--- detectiona.py
+++ detectiona.py
@@ -2,29 +2,46 @@ import cv2
 import numpy
 from collections import namedtuple
 
+
+class Person:
+
+    __slots__ = ['rect', 'optical_dir', 'optical_dir_hist', 'rect_hist', 'rect_vel_smoothed']
+
+    def __init__(self, rect, dir):
+        self.rect = rect
+        self.rect_hist = []
+        self.rect_vel_smoothed = (0,0)
+        self.optical_dir = dir
+        self.optical_dir_hist = []
+
+    def __repr__(self):
+        from pprint import pformat
+        return pformat(
+            [self.rect, self.optical_dir, self.optical_dir_hist, self.rect_hist, self.rect_vel_smoothed]
+            , indent=4, width=1)
+
 prev_frame = None
 hsv = None
 mhi = None
-
-class Person:
-    __slots__ = ['rect', 'dir', 'dirHist']
-    def __init__(self, rect, dir):
-        self.rect = rect
-        self.dir = dir
-        self.dirHist = []
-    def __repr__(self):
-        from pprint import pformat
-        return pformat([self.rect, self.dir], indent=4, width=1)
 person = Person(None, None)
-
-# initialize the HOG descriptor/person detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+hog = None
+term_crit = None
 
 def init():
+    global prev_frame, hsv, mhi, person, hog, term_crit
+
+    prev_frame = None
+    hsv = None
+    mhi = None
+    person = Person(None, None)
+
+    # initialize the HOG descriptor/person detector
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
     pass
 
 
@@ -38,7 +55,7 @@ def parse_frame(frame):
 
     # Downsample for performance
     # Reduce to luminance only for optical flow
-    frame_bw = frame.deionized
+    frame_bw = frame.median
     frame_bw = cv2.cvtColor(frame_bw, cv2.COLOR_BGR2GRAY)
 
     if prev_frame is None:
@@ -75,25 +92,55 @@ def parse_frame(frame):
     if len(rects) > 0:
         pedcolour = (0, 255, 0)
         person.rect = tuple(rects[0])
+        person.rect_hist.append(person.rect)
     elif person.rect is not None:
         pedcolour = (0, 255, 255)
         _, person.rect = cv2.CamShift(frame.fg, person.rect, term_crit)
+        person.rect_hist.append(person.rect)
 
     # If found, estimate person's velocity
     if person.rect is not None:
         (x, y, w, h) = person.rect
-        roi = flow[y:y+h,x:x+w]
-        mean = numpy.mean(roi, axis=(0, 1))
-        person.dir = tuple(mean)
-        person.dirHist.append(mean)
+        roi = flow[y:y + h, x:x + w]
+        roi_mask = frame.fg[y:y + h, x:x + w]
+        mean = cv2.mean(roi, roi_mask)
+        person.optical_dir = tuple(mean)
+        person.optical_dir_hist.append(mean)
 
-    pedframe = frame.deionized.copy()
+    # Estimate raw rect velocity with moving average
+    if len(person.rect_hist) > 4:
+        avg = (0, 0)
+        for i in range(0, 3):
+            dir = numpy.subtract(middle(person.rect_hist[-i-1]), middle(person.rect_hist[-i]))
+            person.rect_vel_smoothed = numpy.add(avg, dir)
+        avg = person.rect_vel_smoothed / 4
+        person.rect_vel_smoothed = avg
+
+    # debug info
+    pedframe = frame.median.copy()
     if person.rect is not None:
+
+        # show enclosing rect
         (x, y, w, h) = person.rect
-        r_start = (int(x + w/2), int(y + h/2))
-        r_end = (int(r_start[0] + person.dir[0] * 10), int(r_start[1] + person.dir[1] * 10))
         cv2.rectangle(pedframe, (x, y), (x + w, y + h), pedcolour, 2)
-        cv2.arrowedLine(pedframe, r_start, r_end, (255,255,255), 2)
+
+        # show optical dir
+        r_start = middle(person.rect)
+        r_end = (int(r_start[0] + person.optical_dir[0] * 10), int(r_start[1] + person.optical_dir[1] * 10))
+        cv2.arrowedLine(pedframe, r_start, r_end, (255,255,255), 2, 3)
+
+        # show pos history
+        for i in range(0, len(person.rect_hist) - 1):
+            cv2.line(pedframe, middle(person.rect_hist[i]), middle(person.rect_hist[i+1]), (255, 0, 0), 2)
+
+        # show rect dir
+        if not tuple(person.rect_vel_smoothed) == (0, 0):
+            a_start = middle(person.rect)
+            a_end = numpy.add(a_start, person.rect_vel_smoothed)
+            a_end = numpy.add(a_end, person.rect_vel_smoothed)
+            cv2.arrowedLine(pedframe, tuple(a_start), (int(a_end[0]), int(a_end[1])), (128, 128, 128), 2, 3)
+
+
     imsToShow.append(pedframe)
 
     if imsToShow is not []:
@@ -104,3 +151,6 @@ def parse_frame(frame):
     DetectionAResults = namedtuple('DetectionAResults', 'person')
     return DetectionAResults(person)
 
+
+def middle(rect):
+    return int(rect[0] + rect[2] / 2), int(rect[1] + rect[3] / 2)
