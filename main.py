4d4d749006ec3cf8b06f2e6f024f7f47146a34d5
AndrewRenn <andrewwrenn@outlook.com>
2017-10-25 23:26:34 +1100

    Detection A Reasoning working

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import reasoning
 import cv2
 import sys
 import os
-vidlist =("b1", "b2", "fw1", "fw2", "fr1", "fr2", "obs1", "obs2", "obs3", "obs4", "obs5", "obs6", "obs7", "obs8", "shoes1", "shoes2")
-fallstart = (20, 20, 68, 71, 60, 23, 60, 60, 1, 1, 58, 0, 136, 103, 48, 10)
-fallend = (60, 67, 133, 116, 120, 77, 102, 119, 28, 42, 92, 0, 184, 158, 148, 124)
+# vidlist =("b1", "b2", "fw1", "fw2", "fr1", "fr2", "obs1", "obs2", "obs3", "obs4", "obs5", "obs6", "obs7", "obs8", "shoes1", "shoes2")
+# fallstart = (20, 20, 68, 71, 60, 23, 60, 60, 1, 1, 58, 0, 136, 103, 48, 10)
+# fallend = (60, 67, 133, 116, 120, 77, 102, 119, 28, 42, 92, 0, 184, 158, 148, 124)
 
 def main():
-    count = 0
     if len(sys.argv) <= 1:
         print('No folder with video provided, playing ALL')
         videos_dir = os.path.dirname(os.path.realpath(__file__)) + '/videos/'
@@ -18,14 +17,13 @@ def main():
             if not os.path.isdir(videos_dir + dir):
 
                 continue
-            play(dir, count)
-            count += 1;
+            play(dir)
     else:
         for i in range(1, len(sys.argv)):
-            play(sys.argv[i], count)
+            play(sys.argv[i])
 
 
-def play(folder, count):
+def play(folder):
     print('Loading ' + folder)
     video = load(folder)
 
@@ -44,10 +42,9 @@ def play(folder, count):
         cleaned_frame = corrections.clean_frame(frame)
         detection_result_a = detectiona.parse_frame(cleaned_frame)
         detection_result_b = detectionb.parse_frame(cleaned_frame)
-        result, ldata, rdata, adata, delLdata, delRdata, delAdata, aveAdata, aveRdata = reasoning.reason(cleaned_frame, frame, detection_result_a, detection_result_b)
+        result = reasoning.reason(cleaned_frame, frame, detection_result_a, detection_result_b)
         cv2.imshow("Result", result)
         cv2.waitKey(1)
-    reasoning.end(ldata, rdata, adata, delLdata, delRdata, delAdata, aveAdata, aveRdata, fallstart[count], fallend[count], vidlist[count])
 
 
 def load(folder_path):
--- reasoning.py
+++ reasoning.py
@@ -2,6 +2,7 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+import cv2
 
 def init():
     pass
@@ -9,7 +10,20 @@ def init():
 
 def reason(frame, originalFrame, detection_results_a, detection_results_b):
 
-    # library and dataset
+    # Threshold values
+    vector_Magnitude_Threshold = 3
+    deltaMagnitudeFallenThreshold = 0.1
+    deltaRatioThreshold = 0.1
+    deltaRatioFallenThreshold = 0.0015
+    ratioThreshold = 0.5
+    ratioFallenThreshold = 1.15
+    aveDeltaAreaThreshold = 250
+    aveDeltaAreaFallenThreshold = 13
+    deltaVertMagThreshold = 3
+
+    #some colours
+    colourArr = ((255,0,0) ,(0,255,0), (0,0,255), (255,255,0), (255,0,255), (0,255,255),
+                (255,125,0), (255,0,125), (125,255,0), (0,255,125), (125,0,255), (0,125,255), (255,255,255),)
 
     directions = detection_results_a.person.optical_dir_hist
     xAxisLength = len(directions)
@@ -18,14 +32,23 @@ def reason(frame, originalFrame, detection_results_a, detection_results_b):
     rectangles = detection_results_a.person.rect_hist
     ratios = np.zeros(shape = (xAxisLength), dtype = float)
     areas = np.zeros(shape = (xAxisLength), dtype = float)
+    verticalMagnitudes = np.zeros(shape = (xAxisLength), dtype = float)
 
     if (xAxisLength > 0):
+        (x2, y2, w2, h2) = detection_results_a.person.rect
+        x = x2 * 2
+        y = y2 * 2
+        w = w2 * 2
+        h = h2 * 2
         deltaLengths = np.zeros(shape = (xAxisLength-1), dtype = float)
         deltaAreas = np.zeros(shape = (xAxisLength-1), dtype = float)
         deltaRatios = np.zeros(shape = (xAxisLength-1), dtype = float)
+        deltaVertMags = np.zeros(shape = (xAxisLength-1), dtype = float)
     if (xAxisLength > 3):
         averageAreas = np.zeros(shape = (xAxisLength-4), dtype = float)
         averageRatios = np.zeros(shape = (xAxisLength-4), dtype = float)
+    if (xAxisLength > 4):
+        averageDeltaAreas = np.zeros(shape = (xAxisLength-5), dtype = float)
 
 
     for i in range (0, xAxisLength):
@@ -43,15 +66,87 @@ def reason(frame, originalFrame, detection_results_a, detection_results_b):
         else:
             ratios[i] = deltaX/deltaY
 
+        if (directions[i][0] == 0):
+            vertMag = 0
+        else:
+            verticalMagnitudes[i] = directions[i][1]/abs(directions[i][0])
+
         areas[i] = deltaX*deltaY
 
         if (i > 0):
             deltaLengths[i-1] = lengths[i] - lengths[i-1]
             deltaRatios[i-1] = ratios[i] - ratios[i-1]
             deltaAreas[i-1] = areas[i] - areas[i-1]
+            deltaVertMags[i-1] = verticalMagnitudes[i] - verticalMagnitudes[i-1]
         if (i > 3):
             averageRatios[i-4] = (ratios[i] + ratios[i-1] + ratios[i-2] + ratios[i-3] + ratios[i-4])/5
             averageAreas[i-4] = (areas[i] + areas[i-1] + areas[i-2] + areas[i-3] + areas[i-4])/5
+        if (i > 4):
+            averageDeltaAreas[i-5] = (deltaAreas[i-1] + deltaAreas[i-2] + deltaAreas[i-3] + deltaAreas[i-4] + deltaAreas[i-5])/5
+
+    #Draw some bounding boxes for clear testing, comment out during final product launch
+    colref = ((0,0,0),(255,255,255))
+    if (xAxisLength > 0):
+        for j in range (0,7):
+            cv2.rectangle(originalFrame[0], (x - (j*2), y - (j*2)), (x + w + (j*2), y + h + (j*2)), colref[((j+1)%2)], 2)
+
+    duringFall = False
+    afterFall = False
+
+    deltaRatioPass = False
+    deltaAreasPass = False
+    deltaVertMagsPass = False
+
+    VectorPass = False
+
+        # quick increase in Ratio Value
+    if (xAxisLength > 1):
+        # print(xAxisLength)
+        # print(ratios)
+        #input("\n Press Enter to Continue... \n")
+        print("Frame: ", xAxisLength)
+        #check to see if the ratio of sides of the bounding rectangle is passing a threshold value
+        if (ratios[xAxisLength-1] > ratioFallenThreshold):
+            print ("FALLEN FIRST CHECK: ratios greater than 1:1")
+            cv2.rectangle(originalFrame[0], (x, y), (x + w, y + h), colourArr[0], 2)
+            if (deltaRatios[xAxisLength-2] < deltaRatioFallenThreshold):
+                print ("AFTER FALL: Low Delta Ratio")
+                cv2.rectangle(originalFrame[0], (x - 2, y - 2), (x + w + 2, y + h + 2), colourArr[1], 2)
+                deltaRatioPass = True
+            if (deltaVertMags[xAxisLength-2] < deltaMagnitudeFallenThreshold):
+                print ("AFTER FALL: Low Delta Magnitude")
+                cv2.rectangle(originalFrame[0], (x - 4, y - 4), (x + w + 4, y + h + 4), colourArr[2], 2)
+                deltaVertMagsPass = True
+            if (xAxisLength > 5):
+                if (averageDeltaAreas[xAxisLength-6] < aveDeltaAreaFallenThreshold):
+                    print ("AFTER FALL: Low Average Delta Areas")
+                    cv2.rectangle(originalFrame[0], (x - 6, y - 6), (x + w + 6, y + h + 6), colourArr[3], 2)
+                    deltaAreasPass = True
+
+        #check to see if the length of the Vector is passing a threshold value
+        if (lengths[xAxisLength-1] > vector_Magnitude_Threshold):
+            # If the vector is above a certain magnitude
+            print ("Vector magnitude pass")
+            if (deltaVertMags[xAxisLength-2] > deltaVertMagThreshold):
+                # And the Vector is pointing downwards
+                print ("FALLING: Vector direction pass")
+                cv2.rectangle(originalFrame[0], (x - 8, y - 8), (x + w + 8, y + h + 8), colourArr[4], 2)
+
+        if (deltaRatios[xAxisLength-2] > deltaRatioThreshold):
+            print ("FALLING: Delta Ratio pass")
+            cv2.rectangle(originalFrame[0], (x - 10, y - 10), (x + w + 10, y + h + 10), colourArr[5], 2)
+
+        if (xAxisLength > 5):
+            if (averageDeltaAreas[xAxisLength-6] > aveDeltaAreaThreshold):
+                print ("FALLING: Average Delta Areas")
+                cv2.rectangle(originalFrame[0], (x - 12, y - 12), (x + w + 12, y + h + 12), colourArr[6], 2)
+
+    # Make the call wether they are falling or have fallen
+
+    # for Fallen, use a best-two-out-of-three case looking for certain values
+    if (((deltaRatioPass or deltaVertMagsPass) and deltaAreasPass) or (deltaRatioPass and deltaVertMagsPass)):
+        Fallen = True
+        cv2.rectangle(originalFrame[0], (x, y), (x + w, y + h), colourArr[2], 8)
 
     # Create data
     yAxis = range(1,xAxisLength+1)
@@ -59,8 +154,8 @@ def reason(frame, originalFrame, detection_results_a, detection_results_b):
     aveYAxis = range(1,xAxisLength-3)
     # angleData = pd.DataFrame({'frames': yAxis, 'angles': angles })
     lengthData = pd.DataFrame({'frames': yAxis, 'vector magnitude': lengths })
-    ratioData=pd.DataFrame({'frames': yAxis, 'ratios of sides': ratios })
-    areaData=pd.DataFrame({'frames': yAxis, 'areas': areas })
+    ratioData = pd.DataFrame({'frames': yAxis, 'ratios of sides': ratios })
+    areaData = pd.DataFrame({'frames': yAxis, 'areas': areas })
 
     if (xAxisLength > 0):
         lengthChangeData = pd.DataFrame({'frames': deltaYAxis, 'delta magnitude': deltaLengths })
@@ -86,74 +181,75 @@ def reason(frame, originalFrame, detection_results_a, detection_results_b):
 
 
 
-    return originalFrame[0], lengthData, ratioData, areaData, lengthChangeData, areaChangeData, ratioChangeData, areaAverageData, ratioAverageData
+    return originalFrame[0]#, lengthData, ratioData, areaData, lengthChangeData, areaChangeData, ratioChangeData, areaAverageData, ratioAverageData
     #return originalFrame[0], angleData, lengthData, ratioData, areaData
 
-def end(lengthData, ratioData, areaData, lengthChangeData, areaChangeData, ratioChangeData, areaAverageData, ratioAverageData, startfall, endfall, videoName):
+#def end(lengthData, ratioData, areaData, lengthChangeData, areaChangeData, ratioChangeData, areaAverageData, ratioAverageData, startfall, endfall, videoName):
+    #pass
     # angleGraph = plt.plot( 'frames', 'angles', data=angleData, marker='o', color='mediumvioletred')
     # angleGraph.append(plt.plot([startfall, startfall], [-1.5, 1.5], color='k', linestyle='-', linewidth=2))
     # angleGraph.append(plt.plot([endfall, endfall], [-1.5, 1.5], color='k', linestyle='-', linewidth=2))
     # plt.show()
 
-    print("Vector Magnitude Graph for video: " + videoName)
-    vectorGraph = plt.plot( 'frames', 'vector magnitude', data=lengthData, marker='o', color='red')
-    vectorGraph.append(plt.plot([startfall, startfall], [5, 0], color='k', linestyle='-', linewidth=2))
-    vectorGraph.append(plt.plot([endfall, endfall], [5, 0], color='k', linestyle='-', linewidth=2))
-    # vectorGraph.set_title("Vector Magnitudes")
-    # vectorGraph.set_xlabel("Frame")
-    # vectorGraph.set_ylabel("Magnitude")
-    plt.show()
-
-    print("Change in magnitude Graph for video: " + videoName)
-    plt.plot( 'frames', 'delta magnitude', data=lengthChangeData, marker='o', color='mediumvioletred')
-    plt.plot([startfall, startfall], [2, 0], color='k', linestyle='-', linewidth=2)
-    plt.plot([endfall, endfall], [2, 0], color='k', linestyle='-', linewidth=2)
-    plt.show()
-
-    print("Ratio of sides Graph for video: " + videoName)
-    ratioGraph = plt.plot( 'frames', 'ratios of sides', data=ratioData, marker='o', color='cyan')
-    ratioGraph.append(plt.plot([startfall, startfall], [2, 0], color='k', linestyle='-', linewidth=2))
-    ratioGraph.append(plt.plot([endfall, endfall], [2, 0], color='k', linestyle='-', linewidth=2))
-    # ratioGraph.set_title("Ratio of Width to Height")
-    # ratioGraph.set_xlabel("Frame")
-    # ratioGraph.set_ylabel("Ratio W/H")
-    plt.show()
-
-    print("Average Ratio Graph for video: " + videoName)
-    plt.plot( 'frames', 'average ratios', data=ratioAverageData, marker='o', color='navy')
-    plt.plot([startfall, startfall], [1, 0], color='k', linestyle='-', linewidth=2)
-    plt.plot([endfall, endfall], [1, 0], color='k', linestyle='-', linewidth=2)
-    plt.show()
-
-    print("Change in Ratio Graph for video: " + videoName)
-    plt.plot( 'frames', 'delta ratios', data=ratioChangeData, marker='o', color='blue')
-    plt.plot([startfall, startfall], [1, 0], color='k', linestyle='-', linewidth=2)
-    plt.plot([endfall, endfall], [1, 0], color='k', linestyle='-', linewidth=2)
-    plt.show()
-
-    print("Area Graph for video: " + videoName)
-    areaGraph = plt.plot( 'frames', 'areas', data=areaData, marker='o', color='g')
-    areaGraph.append(plt.plot([startfall, startfall], [10000, 0], color='k', linestyle='-', linewidth=2))
-    areaGraph.append(plt.plot([endfall, endfall], [10000, 0], color='k', linestyle='-', linewidth=2))
-    # areaGraph.set_title("Area of bouncing rectangle")
-    # areaGraph.set_xlabel("Frame")
-    # areaGraph.set_ylabel("Area")
-    plt.show()
-
-    print("Average Area Graph for video: " + videoName)
-    areaGraph = plt.plot( 'frames', 'average areas', data=areaAverageData, marker='o', color='chartreuse')
-    areaGraph.append(plt.plot([startfall, startfall], [10000, 0], color='k', linestyle='-', linewidth=2))
-    areaGraph.append(plt.plot([endfall, endfall], [10000, 0], color='k', linestyle='-', linewidth=2))
-    # areaGraph.set_title("Area of bouncing rectangle")
-    # areaGraph.set_xlabel("Frame")
-    # areaGraph.set_ylabel("Area")
-    plt.show()
-
-    print("Chage of Area Graph for video: " + videoName)
-    plt.plot( 'frames', 'delta areas', data=areaChangeData, marker='o', color='lime')
-    # plt.plot([startfall, startfall], [np.amin(areaChangeData.values, axis = 1), np.amax(areaChangeData.values, axis = 1)], color='k', linestyle='-', linewidth=2)
-    # plt.plot([endfall, endfall], [np.amin(areaChangeData.values, axis = 1), np.amax(areaChangeData.values, axis = 1)], color='k', linestyle='-', linewidth=2)
-    plt.show()
+    # print("Vector Magnitude Graph for video: " + videoName)
+    # vectorGraph = plt.plot( 'frames', 'vector magnitude', data=lengthData, marker='o', color='red')
+    # vectorGraph.append(plt.plot([startfall, startfall], [5, 0], color='k', linestyle='-', linewidth=2))
+    # vectorGraph.append(plt.plot([endfall, endfall], [5, 0], color='k', linestyle='-', linewidth=2))
+    # # vectorGraph.set_title("Vector Magnitudes")
+    # # vectorGraph.set_xlabel("Frame")
+    # # vectorGraph.set_ylabel("Magnitude")
+    # plt.show()
+    #
+    # print("Change in magnitude Graph for video: " + videoName)
+    # plt.plot( 'frames', 'delta magnitude', data=lengthChangeData, marker='o', color='mediumvioletred')
+    # plt.plot([startfall, startfall], [2, 0], color='k', linestyle='-', linewidth=2)
+    # plt.plot([endfall, endfall], [2, 0], color='k', linestyle='-', linewidth=2)
+    # plt.show()
+    #
+    # print("Ratio of sides Graph for video: " + videoName)
+    # ratioGraph = plt.plot( 'frames', 'ratios of sides', data=ratioData, marker='o', color='cyan')
+    # ratioGraph.append(plt.plot([startfall, startfall], [2, 0], color='k', linestyle='-', linewidth=2))
+    # ratioGraph.append(plt.plot([endfall, endfall], [2, 0], color='k', linestyle='-', linewidth=2))
+    # # ratioGraph.set_title("Ratio of Width to Height")
+    # # ratioGraph.set_xlabel("Frame")
+    # # ratioGraph.set_ylabel("Ratio W/H")
+    # plt.show()
+    #
+    # print("Average Ratio Graph for video: " + videoName)
+    # plt.plot( 'frames', 'average ratios', data=ratioAverageData, marker='o', color='navy')
+    # plt.plot([startfall, startfall], [1, 0], color='k', linestyle='-', linewidth=2)
+    # plt.plot([endfall, endfall], [1, 0], color='k', linestyle='-', linewidth=2)
+    # plt.show()
+    #
+    # print("Change in Ratio Graph for video: " + videoName)
+    # plt.plot( 'frames', 'delta ratios', data=ratioChangeData, marker='o', color='blue')
+    # plt.plot([startfall, startfall], [1, 0], color='k', linestyle='-', linewidth=2)
+    # plt.plot([endfall, endfall], [1, 0], color='k', linestyle='-', linewidth=2)
+    # plt.show()
+    #
+    # print("Area Graph for video: " + videoName)
+    # areaGraph = plt.plot( 'frames', 'areas', data=areaData, marker='o', color='g')
+    # areaGraph.append(plt.plot([startfall, startfall], [10000, 0], color='k', linestyle='-', linewidth=2))
+    # areaGraph.append(plt.plot([endfall, endfall], [10000, 0], color='k', linestyle='-', linewidth=2))
+    # # areaGraph.set_title("Area of bouncing rectangle")
+    # # areaGraph.set_xlabel("Frame")
+    # # areaGraph.set_ylabel("Area")
+    # plt.show()
+    #
+    # print("Average Area Graph for video: " + videoName)
+    # areaGraph = plt.plot( 'frames', 'average areas', data=areaAverageData, marker='o', color='chartreuse')
+    # areaGraph.append(plt.plot([startfall, startfall], [10000, 0], color='k', linestyle='-', linewidth=2))
+    # areaGraph.append(plt.plot([endfall, endfall], [10000, 0], color='k', linestyle='-', linewidth=2))
+    # # areaGraph.set_title("Area of bouncing rectangle")
+    # # areaGraph.set_xlabel("Frame")
+    # # areaGraph.set_ylabel("Area")
+    # plt.show()
+    #
+    # print("Chage of Area Graph for video: " + videoName)
+    # plt.plot( 'frames', 'delta areas', data=areaChangeData, marker='o', color='lime')
+    # # plt.plot([startfall, startfall], [np.amin(areaChangeData.values, axis = 1), np.amax(areaChangeData.values, axis = 1)], color='k', linestyle='-', linewidth=2)
+    # # plt.plot([endfall, endfall], [np.amin(areaChangeData.values, axis = 1), np.amax(areaChangeData.values, axis = 1)], color='k', linestyle='-', linewidth=2)
+    # plt.show()
 
     # plt.plot( 'frames', 'angles', data=angleData, marker='o', color='mediumvioletred')
     # plt.plot([startfall, startfall], [np.amin(angleData.values, axis = 1), np.amax(angleData.values, axis = 1)], color='k', linestyle='-', linewidth=2)
